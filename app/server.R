d99f45269576c269a9fa3be4bfaec8b41afaa9e6
Dipesh Patel <dipeshp1991@gmail.com>
2021-10-20 17:12:12 -0400

    app with plots

--- app/server.R
+++ app/server.R
@@ -6,7 +6,8 @@
 #
 #    http://shiny.rstudio.com/
 #
-###############################Install Related Packages #######################
+#-------------------------------------------------App Server----------------------------------
+
 if (!require("shiny")) {
     install.packages("shiny")
     library(shiny)
@@ -19,186 +20,179 @@ if (!require("leaflet.extras")) {
     install.packages("leaflet.extras")
     library(leaflet.extras)
 }
-if (!require("dplyr")) {
-    install.packages("dplyr")
-    library(dplyr)
-}
 if (!require("magrittr")) {
     install.packages("magrittr")
     library(magrittr)
-}
-if (!require("mapview")) {
-    install.packages("mapview")
-    library(mapview)
 }
 if (!require("leafsync")) {
     install.packages("leafsync")
     library(leafsync)
 }
+if (!require("lubridate")) {
+    install.packages("lubridate")
+    library(lubridate)
+}
+if (!require("tidyverse")) {
+    install.packages("tidyverse")
+    library(tidyverse)
+}
+if (!require("pacman")) {
+    install.packages("pacman")
+    library(pacman)
+}
+if (!require("zipcodeR")) {
+    install.packages("zipcodeR")
+    library(zipcodeR)
+}
+if (!require("data.table")) {
+    install.packages("data.table")
+    library(data.table)
+}
+if (!require("devtools")) {
+    install.packages("devtools")
+    library(devtools)
+}
+if (!require("tigris")) {
+    install.packages("tigris")
+    library(tigris)
+}
+if (!require("htmltools")) {
+    install.packages("htmltools")
+    library(htmltools)
+}
 
-#Data Processing
-total_citi_bike_df = read.csv('../data/citibike_data.csv')
-##compute the daily in and out difference for the station
-total_citi_bike_df$day_diff = total_citi_bike_df$endcount - total_citi_bike_df$startcount
-#assign each column to weekend or weekday
-total_citi_bike_df$weekend_or_weekday = ifelse(total_citi_bike_df$weekday %in% c('Saturday','Sunday'), "Weekend", "Weekday")
+# LOAD AND PREPARE DATA ####################################
 
-#station info
-citi_bike_station_info <- total_citi_bike_df[,c('station_id','station_name','station_longitude','station_latitude')]
-#remove the duplicates based on station id 
-citi_bike_station_info <- citi_bike_station_info[!duplicated(citi_bike_station_info[ , c("station_id")]),]
+# Import CSV files with readr::read_csv() from tidyverse
+df_vl <- data.frame(read_csv("../data/Housing_Maintenance_Code_Violations.csv"))
+df_cm <- data.frame(read_csv("../data/Housing_Maintenance_Code_Complaints.csv"))
 
-#split the bike data to pre-covid and covid time period
-citi_bike_pre_covid_df = total_citi_bike_df[difftime(total_citi_bike_df$date,"2019-05-31")<=0,] #2019-05-01 ~ 2019-05-31
-citi_bike_covid_df = total_citi_bike_df[difftime(total_citi_bike_df$date,"2020-04-30")>=0,] #2020-05-01 ~ 2021-05-31
+# Changing type of values (dates)
+df_vl$InspectionDate <- as.Date(df_vl$InspectionDate, format = '%m/%d/%Y')
+df_cm$ReceivedDate <- as.Date(df_cm$ReceivedDate, format = '%m/%d/%Y')
+
+# Generating key Year-Month
+Y_m_vl <- paste(strftime(df_vl$InspectionDate, "%Y"))
+Y_m_cm <- paste(strftime(df_cm$ReceivedDate, "%Y"))
+
+# Combining data with key
+df_vl <- cbind(df_vl,Y_m_vl)
+df_cm <- cbind(df_cm,Y_m_cm)
+
+# Building counting tables & changing column names
+counting.df_vl <- df_vl %>% 
+    count(Postcode, Y_m_vl) %>% 
+    group_by(Postcode)
+
+colnames(counting.df_vl) <- c("region","key","value")
+
+counting.df_cm <- df_cm %>%
+    count(Zip, Y_m_cm) %>%
+    group_by(Zip)
+
+colnames(counting.df_cm) <- c("region","key","value")
+
+# Filtering actual zipcodes
+counting.df_vl <- filter(counting.df_vl, region > 10000 & region < 12000)
+counting.df_cm <- filter(counting.df_cm, region > 10000 & region < 12000)
+
+counting.df_vl <- filter(counting.df_vl, key > 2017)
+counting.df_cm <- filter(counting.df_cm, key > 2017)
+
+counting.df_vl$region <- as.character(counting.df_vl$region)
+counting.df_cm$region <- as.character(counting.df_cm$region)
+
+# cache zip boundaries that are downloaded via tigris package
+options(tigris_use_cache = TRUE)
+
+zcta <- zctas(starts_with = c(10, 11), year = 2010, state = "New York")
 
 
-# Define server logic required to draw a histogram
+# static plots
+df <- load("../data/2018up.Rda")
+df <- data.2018up
+
+df$date <- paste(year(df$CurrentStatusDate),month(df$CurrentStatusDate),sep="-")
+
+# covid data
+covid_df = read.csv("../data/COVID-19_Daily_Counts_of_Cases__Hospitalizations__and_Deaths.csv")
+covid_df$DATE <- as.Date(covid_df$DATE_OF_INTEREST, format = "%m/%d/%Y")
+
 shinyServer(function(input, output) {
-
-    ## Map Tab section
+    issue <- reactive({
+        if (input$issue == "violations") {
+            return(filter(counting.df_vl, key == input$iyear))
+        }
+        if (input$issue == "complaints") {
+            return(filter(counting.df_cm, key == input$iyear))
+        }
+    })
     
-    output$left_map <- renderLeaflet({
-    
-    #adjust for weekday/weekend effect
-    if (input$adjust_time =='Overall') {
-        leaflet_plt_df <- citi_bike_pre_covid_df %>% 
-                            group_by(station_id) %>%
-                            summarise(total_start_count = sum(startcount),
-                                      total_end_count = sum(endcount),
-                                      total_day_diff = sum(day_diff),
-                                      total_diff_percentage = sum(day_diff)/sum(startcount),
-                            ) %>% left_join(citi_bike_station_info,by='station_id')
-    } else {
-        leaflet_plt_df <- citi_bike_pre_covid_df %>% 
-                            filter(weekend_or_weekday == input$adjust_time) %>%
-                            group_by(station_id) %>%
-                                summarise(total_start_count = sum(startcount),
-                                          total_end_count = sum(endcount),
-                                          total_day_diff = sum(day_diff),
-                                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                                ) %>% left_join(citi_bike_station_info,by='station_id')
-                            } 
-
+    output$heatmap <- renderLeaflet({
+        zcta <- geo_join(zcta, issue(), by_sp = "ZCTA5CE10", by_df = "region", how = "left")
+        zcta <- zcta %>% drop_na()
         
-    map_2019 <- leaflet_plt_df %>%
-         leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-         addTiles() %>%
-         addProviderTiles("CartoDB.Positron",
-                          options = providerTileOptions(noWrap = TRUE)) %>%
-         setView(-73.9834,40.7504,zoom = 12)
-     
-     if (input$adjust_score == 'start_cnt') {
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_start_count,
-                        max=4000,
-                        radius=8,
-                        blur=10)
-     }else if (input$adjust_score == 'end_cnt') {
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_end_count,
-                        max=4000,
-                        radius=8,
-                        blur=10)
-     } else if (input$adjust_score == 'day_diff_absolute'){
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_day_diff,
-                        max=50,
-                        radius=8,
-                        blur=10)
-         
-     }else if (input$adjust_score == 'day_diff_percentage'){
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_diff_percentage,#change to total day diff percentage
-                        max=0.1,
-                        radius=8,
-                        blur=10)
-         
-     }
-     }) #left map plot
-    
-    output$right_map <- renderLeaflet({
-        #adjust for weekday/weekend effect
-        if (input$adjust_time =='Overall') {
-            leaflet_plt_df <- citi_bike_covid_df %>% 
-                group_by(station_id) %>%
-                summarise(total_start_count = sum(startcount),
-                          total_end_count = sum(endcount),
-                          total_day_diff = sum(day_diff),
-                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                ) %>% left_join(citi_bike_station_info,by='station_id')
-        } else {
-            leaflet_plt_df <- citi_bike_covid_df %>% 
-                filter(weekend_or_weekday == input$adjust_time) %>%
-                group_by(station_id) %>%
-                summarise(total_start_count = sum(startcount),
-                          total_end_count = sum(endcount),
-                          total_day_diff = sum(day_diff),
-                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                ) %>% left_join(citi_bike_station_info,by='station_id')
-        } 
-        #initial the map to plot on
-        map_2020 <- leaflet_plt_df %>%
-            leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-            addTiles() %>%
-            addProviderTiles("CartoDB.Positron",
-                             options = providerTileOptions(noWrap = TRUE)) %>%
-            setView(-73.9834,40.7504,zoom = 12) 
-        
-        if (input$adjust_score == 'start_cnt') {
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                            intensity=~total_start_count, #change to total start count
-                            max=4000,
-                            radius=8,
-                           blur=10)
-        }else if (input$adjust_score == 'end_cnt') {
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_end_count,#change to total end count
-                           max=4000,
-                           radius=8,
-                           blur=10)
-        } else if (input$adjust_score == 'day_diff_absolute'){
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_day_diff,#change to total day diff
-                           max=50,
-                           radius=8,
-                           blur=10)
-            
-        }else if (input$adjust_score == 'day_diff_percentage'){
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_diff_percentage,#change to total day diff percentage
-                           max=0.1,
-                           radius=8,
-                           blur=10)
-            
+        if (input$issue == "violations") {
+            pal <- colorNumeric(
+                palette = "Greens",
+                domain = zcta$value)
         }
         
-    }) #right map plot
-
+        if (input$issue == "complaints") {
+            pal <- colorNumeric(
+                palette = "Blues",
+                domain = zcta$value)
+        }
+        
+        labels <- 
+            paste0(
+                "zip code: ",
+                zcta$ZCTA5CE10, "<br/>", input$issue,
+                ": ",
+                zcta$value) %>%
+            lapply(htmltools::HTML)
+        
+        zcta %>% 
+            leaflet %>% 
+            # add base map
+            addProviderTiles("CartoDB") %>% 
+            # add zip codes
+            addPolygons(fillColor = ~pal(value),
+                        weight = 2,
+                        opacity = 1,
+                        color = "white",
+                        dashArray = "3",
+                        fillOpacity = 0.7,
+                        highlight = highlightOptions(weight = 2,
+                                                     color = "#666",
+                                                     dashArray = "",
+                                                     fillOpacity = 0.7,
+                                                     bringToFront = TRUE),
+                        label = labels) %>%
+            addLegend(pal = pal,
+                      values = ~value,
+                      opacity = 0.7, 
+                      title = htmltools::HTML(paste0(input$issue, "<br> 
+                                    by zip code")),
+                      position = "bottomright")
+    })
+    
+    output$static <- renderPlot({
+        ggplot(data=df) +
+            geom_bar(aes(x=factor(.data[[input$db]]),fill=factor(ViolationStatus)))+
+            labs(title = "Housing Violations",x=input$db,y="Violation Count",fill="Violation Status")+
+            theme(plot.title = element_text(hjust = 0.5),
+                  axis.text.x = element_text(hjust = 0.5, angle = 45),
+                  legend.position = c(0.88, 0.75))
+    })
+    
+    output$covid <- renderPlot({
+        ggplot(data = covid_df)+
+            geom_point(mapping = aes(x=DATE, y=CASE_COUNT))+
+            geom_line(mapping = aes(x=DATE, y=CASE_COUNT))+
+            labs(x="Date",y="Cases", title ="Covid-19 Case Count")+
+            scale_x_date(date_breaks = "1 month")+
+            theme(plot.title = element_text(hjust = 0.5),
+                  axis.text.x = element_text(hjust = 0.5, angle = 45))
+    })
 })
-
-
--- app/ui.R
+++ app/ui.R
@@ -1,15 +1,6 @@
-
 if (!require("shiny")) {
   install.packages("shiny")
   library(shiny)
-}
-if (!require("shinyWidgets")) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
-}
-if (!require("shinythemes")) {
-  install.packages("shinythemes")
-  library(shinythemes)
 }
 if (!require("leaflet")) {
   install.packages("leaflet")
@@ -20,55 +11,89 @@ if (!require("leaflet.extras")) {
   library(leaflet.extras)
 }
 
-# Define UI for application that draws a histogram
 shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
-                                             leafletOutput("right_map",width="100%",height=1200))),
-                        #control panel on the left
-                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                      tags$h4('Citi Bike Activity Comparison'), 
-                                      tags$br(),
-                                      tags$h5('Pre-covid(Left) Right(Right)'), 
-                                      prettyRadioButtons(
-                                                      inputId = "adjust_score",
-                                                      label = "Score List:", 
-                                                      choices = c("start_cnt", 
-                                                                  "end_cnt", 
-                                                                  "day_diff_absolute",
-                                                                  "day_diff_percentage"),
-                                                      inline = TRUE, 
-                                                      status = "danger",
-                                                      fill = TRUE
-                                                        ),
-                                      awesomeRadio("adjust_time", 
-                                                   label="Time",
-                                                    choices =c("Overall",
-                                                               "Weekday", 
-                                                               "Weekend"), 
-                                                    selected = "Overall",
-                                                    status = "warning"),
-                                      # selectInput('adjust_weather',
-                                      #             label = 'Adjust for Weather',
-                                      #             choices = c('Yes','No'), 
-                                      #             selected = 'Yes'
-                                      #             ),
-                                      style = "opacity: 0.80"
-                                      
-                                ), #Panel Control - Closing
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
-   
-
-
-    ) #navbarPage closing  
-) #Shiny UI closing    
+  navbarPage(
+    "Housing Violations",
+    #################### tab 1 ####################
+    tabPanel(
+      "Introduction",
+      tags$img(
+        src = "https://nypost.com/wp-content/uploads/sites/2/2021/02/nyc-housing-forecast-prices-market.jpg?quality=80&strip=all",
+        width = "100%",
+        style = "opacity: 0.90"
+      ),
+      fluidRow(
+        absolutePanel(
+          style = "background-color: white",
+          top = "40%",
+          left = "25%",
+          right = "25%",
+          height = 170,
+          tags$p(
+            style = "padding: 5%; background-color: white; font-family: alegreya; font-size: 120%",
+            "The Covid-19 pandemic has had a profound impact on many aspects of daily life in New York City. Due to rising cases and several lockdowns, business and governmental work slowed. Here we look at how housing maintenance code violations were handled by the Department of Housing Preservation and Development (HPD) during this time."
+          )
+        )
+      )
+    ),
+    #################### tab 2 ####################
+    tabPanel(
+      "Static Plots",
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("db", "By", c("date", "Borough"))
+        ),
+        mainPanel(
+          plotOutput("static"),
+          plotOutput("covid")
+        )
+      )
+    ),
+    #################### tab 3 ####################
+    tabPanel(
+      "Interactive Plot",
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("issue", "Issue", c("violations", "complaints")),
+          selectInput("iyear", "Year", c(2018, 2019, 2020, 2021))
+        ),
+        mainPanel(
+          leafletOutput("heatmap")
+        )
+      )
+    ),
+    #################### tab 4 ####################
+    tabPanel(
+      "References",
+      tags$h2(
+        "Data Sources"
+      ),
+      tags$a(
+        href = "https://data.cityofnewyork.us/Housing-Development/Housing-Maintenance-Code-Violations/wvxf-dwi5",
+        "https://data.cityofnewyork.us/Housing-Development/Housing-Maintenance-Code-Violations/wvxf-dwi5"
+      ),
+      tags$h2(
+        "Contributors"
+      ),
+      tags$p(
+        "Arya Ayati-Ghaffari"
+      ),
+      tags$p(
+        "Dipesh Patel"
+      ),
+      tags$p(
+        "Rodrigo Zarate"
+      ),
+      tags$p(
+        "Yang Zhao"
+      ),
+      tags$h2(
+        "GitHub Repository"
+      ),
+      tags$a(
+        href = "https://github.com/TZstatsADS/Fall2021-Project2-group1",
+        "https://github.com/TZstatsADS/Fall2021-Project2-group1"
+      )
+    )
+  ) # end of navbar
+) # end of ui
